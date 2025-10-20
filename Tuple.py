3d87f58e3c01dc59bce5d6242dabcbf589d55cf2
ella-ni <138303518+ella-ni@users.noreply.github.com>
2025-10-20 09:30:32 +0300

    Add files via upload

---
+++ Tuple.py
@@ -0,0 +1,22 @@
+# immutable data types (can not change)
+# similar to lists, but you can't add/remove items
+
+tuple_1 = (5, 6, 8, 9)
+tuple_2 = (8, "f", "string", 55)
+
+# a tuple with one element
+tuple_3 = ("gh",)
+
+# empty tuple
+tuple_4 = ()
+
+# built-in functions for tuples
+# number of elements in tuple
+print(len(tuple_1))
+
+# converting a list into tuple or CASTING
+# tuple(list)
+
+# max value
+print(max(tuple_1))
+
