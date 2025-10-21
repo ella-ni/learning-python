b5b1f60dc841aebe9d294a08138987033ba72091
ella-ni <138303518+ella-ni@users.noreply.github.com>
2025-10-21 15:09:15 +0300

    Add files via upload

---
+++ Booleans_and_Operators.py
@@ -0,0 +1,45 @@
+# Booleans are built-in data type. 
+# TRUE OR FALSE
+
+# Boolean operators are
+#   AND: requires both values to be TRUE for result to be TRUE
+#   OR: requires one of the values  to be TRUE for the result to be TRUE
+#   NOT: negates the value that follows it
+
+print(True and True)
+print(True and False)
+#  with OR Python won't even look at the second option, if the first one is already TRUE
+print(True or False) 
+print(True or True)
+print(not True)
+print(not False)
+print(True and not False)
+
+# Types of operators: arithmetic, comparison and logical operators
+
+print(2 * 5)
+
+#  ** power
+print(4 ** 2)
+
+#  % modulus
+print(5 % 2)
+print(8 % 2)
+
+#  // division floor
+print(8 / 3)
+print(8 // 3)
+
+# logical operators
+print(5 == 5)
+print(5 == 8)
+print(5 != 5)
+print(5 != 8)
+
+
+6 == len('python') and '234'.isdigit()
+
+
+
+
+
